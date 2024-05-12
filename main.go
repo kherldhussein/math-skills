7a79315073d701827ef4217497cc4313e939c1b9
khahussein <kherld.hussein@gmail.com>
2024-05-12 04:11:47 +0300

    Feat: Call helper functions to calculate various statistical measures average median variance standard deviation on the data

--- main.go
+++ main.go
@@ -11,7 +11,17 @@ func main() {
 		log.Print("usage: go run . data.txt")
 		return
 	}
+
 	file := os.Args[1]
 	data := ReadData(file)
-	fmt.Println(data)
+
+	mean := Average(data)
+	median := Median(data)
+	variance := Variance(data)
+	sd := StandardDiviation(data)
+
+	fmt.Printf("Average: %v\n", mean)
+	fmt.Printf("Median: %v\n", median)
+	fmt.Printf("Variance: %v\n", variance)
+	fmt.Printf("Standard Deviation: %v\n", sd)
 }
