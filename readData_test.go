3dea5b8248b7cb7f3255f7eecb9cc18347000468
khahussein <kherld.hussein@gmail.com>
2024-05-17 17:22:32 +0300

    test: Implement Read Data test

--- readData_test.go
+++ readData_test.go
@@ -10,16 +10,16 @@ func TestReadData(t *testing.T) {
 		path string
 	}
 	tests := []struct {
-		name string
-		args args
-		want []float64
+		name     string
+		args     args
+		expected []float64
 	}{
-		// TODO: Add test cases.
+		{name: "Reading", args: args{path: "data.txt"}, expected: []float64{189, 113, 121, 114, 145, 110}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ReadData(tt.args.path); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ReadData() = %v, want %v", got, tt.want)
+			if got := ReadData(tt.args.path); !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("ReadData() = %v, expected %v", got, tt.expected)
 			}
 		})
 	}
