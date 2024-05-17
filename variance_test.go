5053492bf57e0acf4f11f05829811d566d5f6423
khahussein <kherld.hussein@gmail.com>
2024-05-17 17:33:15 +0300

    fix test: Implement Variance test

--- variance_test.go
+++ variance_test.go
@@ -11,12 +11,19 @@ func TestVariance(t *testing.T) {
 		args     args
 		expected int
 	}{
-		{name: "Multiple elements", args: args{[]float64{1.0, 2.0, 3.0}}, expected: 1},
+		{name: "Multiple elements", args: args{[]float64{
+			189,
+			113,
+			121,
+			114,
+			145,
+			110,
+		}}, expected: 784},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Variance(tt.args.x); got != tt.expected {
-				t.Errorf("Variance() = %v, want %v", got, tt.expected)
+				t.Errorf("Variance() = %v, expected %v", got, tt.expected)
 			}
 		})
 	}
